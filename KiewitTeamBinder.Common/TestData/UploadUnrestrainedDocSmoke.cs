b45e935475264fe71e472c800cf1d565b274a22f
Cam.Nguyen <Cam.Nguyen@kiewit.com>
2019-01-04 14:10:07 +0700

    Complete UserStory 120278

---
+++ KiewitTeamBinder.Common/TestData/UploadUnrestrainedDocSmoke.cs
@@ -0,0 +1,31 @@
+﻿using KiewitTeamBinder.Common.Helper;
+using KiewitTeamBinder.Common.Models.VendorData;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiewitTeamBinder.Common.TestData
+{
+    public class UploadUnrestrainedDocSmoke
+    {
+        public string ProjectName = "Automation Project 1";
+        public string FileNames = "\"UnrestrainedDocAttachFile.txt\" ";
+        public string[] RequiredFields = { "Document No.", "Rev", "Status", "Title", "Category", "Discipline" };
+        public Color ColorGrey = Color.FromArgb(207,207,207);
+        public int MaxLengthOfDocNo = 40;
+        public string SaveMessage = "Document details saved successfully.";
+        public SingleDocumentInfo SingleDocInformation = new SingleDocumentInfo()
+        {
+            DocumentNo = Utils.GetRandomValue("Unrestrained DocumentNo"),
+            RevStatus = "F - Rev F",
+            Status = "IFR - Issued for Review",
+            Title = Utils.GetRandomValue("Unrestrained DocumentTitle"),
+            Category = "RE - REFERENCE",
+            Discipline = "87 - Misc Specialty Work",
+            Type = "SUB - Submittal",
+        };
+    }
+}
