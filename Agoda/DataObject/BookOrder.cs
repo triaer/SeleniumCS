e425e36c74b92c7b8250a46b93582a925c84f69e
vinh.tuan.nguyen <vinh.tuan.nguyen@LGVN14704>
2019-04-03 10:42:14 +0700

    april 03 commit 2

--- Agoda/DataObject/BookOrder.cs
+++ Agoda/DataObject/BookOrder.cs
@@ -14,11 +14,16 @@ namespace Agoda.DataObject
         static public int guestNumber = 4;
         static public int roomInNeed = 2;
 
+        public BookOrder()
+        {
+
+        }
+
         public BookOrder(string language = "EN")
         {
             //init
-            if( language.Equals("EN")
-                destination = "Phu Quoc"
+            if (language.Equals("EN"))
+                destination = "Phu Quoc";
         }
     }
 }
